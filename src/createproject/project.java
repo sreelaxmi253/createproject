f8ae1ef6ee59820169a7aed3c543e4bd60681697
sreelaxmi253 <sreelaxmi253@gmail.com>
2019-06-12 22:31:50 +0530

    Second Commit

---
+++ src/createproject/project.java
@@ -0,0 +1,13 @@
+package createproject;
+
+public class project {
+
+	public static void main(String[] args) {
+		
+		int num1=5,num2=9,sum;
+		sum=num1+num2;
+		System.out.println("sum of "+num1+" and "+num2+" is "+sum);
+
+	}
+
+}
